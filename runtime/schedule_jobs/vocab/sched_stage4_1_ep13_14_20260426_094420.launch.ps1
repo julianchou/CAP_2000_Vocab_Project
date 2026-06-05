a9725c4e8eb6c6fbde66cfc90d7a8d640b4f56e0
JulianChou <julian0711@gamil.com>
2026-06-06 03:27:38 +0800

    Modify LLM to NVIDIA

--- runtime/schedule_jobs/vocab/sched_stage4_1_ep13_14_20260426_094420.launch.ps1
+++
@@ -1,4 +0,0 @@
-$ErrorActionPreference = "Stop"
-$env:PYTHONUNBUFFERED = "1"
-& "C:\\Users\\User\\AppData\\Local\\Programs\\Python\\Python311\\python.exe" "-u" "C:\\Project\\CAP_2000_Vocab_Project\\scripts\\run_stage_schedule_background.py" "--root" "C:\\Project\\CAP_2000_Vocab_Project" "--workspace-root" "C:\\Project\\CAP_2000_Vocab_Project\\workspaces\\vocab" "--job-id" "sched_stage4_1_ep13_14_20260426_094420" "--profile-id" "vocab" "--stages-path" "C:\\Project\\CAP_2000_Vocab_Project\\config\\vocab\\stages.yaml"
-exit $LASTEXITCODE
